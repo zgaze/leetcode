fb45079c103b5e00754d77ab96c7adda31cdd01a
zzy <942744575@qq.com>
2019-12-12 18:34:42 +0800

    105. 从中序与前序遍历序列构造二叉树

---
+++ Tree/ConstructBinaryTreefromPreorderandInorderTraversal_105.cpp
@@ -0,0 +1,75 @@
+/* ***********************************************************************
+
+  > File Name: ConstructBinaryTreefromPreorderandInorderTraversal_105.cpp
+  > Author: zzy
+  > Mail: 942744575@qq.com 
+  > Created Time: Mon 12 Aug 2019 11:23:13 AM CST
+
+ ********************************************************************** */
+
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <climits>
+#include <gtest/gtest.h>
+
+using std::vector;
+using std::string;
+
+/*
+ *
+ * 根据一棵树的前序遍历与中序遍历构造二叉树。
+ *
+ * 注意:
+ * 你可以假设树中没有重复的元素。
+ *
+ * 例如，给出
+ *
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ *
+ */
+
+// Definition for a binary tree node.
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int begin, int end, int& index) {
+        if (end < begin) return nullptr;
+        if (index == (int)inorder.size()) return nullptr;
+        int root_val = preorder[index];
+        int i = begin;
+        for (;i <= end; ++i) {
+            if (inorder[i] == root_val) break;
+        }
+        index ++;
+        TreeNode* root = new TreeNode(inorder[i]);
+		// 从开始到根节点前面 的是左子树
+        root->left = build(preorder, inorder, begin, i-1, index);
+        root->right = build(preorder, inorder, i+1, end, index);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.empty()) return nullptr;
+        int index = 0;
+        return build(preorder,inorder,0, preorder.size()-1, index);
+    }
+};
+
+TEST(testCase,test0) {
+
+}
+
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
+
